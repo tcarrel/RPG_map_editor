01e2aced1962921100aead3240c5f02ab7b42027
Thomas Russel Carrel <goshisanniichi@yahoo.com>
2018-03-10 16:14:18 -0800

    Added copy ctors and assignment opperators.

---
+++ RPG/text_line.cpp
@@ -0,0 +1,44 @@
+
+
+#include "stdafx.h"
+
+
+
+Line_of_Text::Line_of_Text( void )
+{
+    this->x = this->y = 0;
+    this->text = "";
+}
+
+
+
+Line_of_Text::Line_of_Text( const Uint8_t_String& str )
+{
+    this->x = this->y = 0;
+    this->text = str;
+}
+
+
+
+Line_of_Text::Line_of_Text( const Line_of_Text& copy_from )
+{
+    *this = copy_from;
+}
+
+
+
+Line_of_Text& Line_of_Text::operator=( const Uint8_t_String& str )
+{
+    this->text = str;
+    return *this;
+}
+
+
+
+Line_of_Text& Line_of_Text::operator=( const Line_of_Text& rhs )
+{
+    this->x = rhs.x;
+    this->y = rhs.y;
+    this->text = rhs.text;
+    return *this;
+}
--- RPG/text_line.h
+++ RPG/text_line.h
@@ -7,4 +7,11 @@ struct Line_of_Text
     int x;
     int y;
     Uint8_t_String text;
+
+    Line_of_Text( void );
+    Line_of_Text( const Uint8_t_String& );
+    Line_of_Text( const Line_of_Text& );
+
+    Line_of_Text& operator=( const Uint8_t_String& );
+    Line_of_Text& operator=( const Line_of_Text& );
 };
