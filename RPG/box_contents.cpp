f7bfe1f709d6b905f0f2e837840d7201d397add7
Thomas Russel Carrel <goshisanniichi@yahoo.com>
2018-03-10 17:08:06 -0800

    Completed the initial form of the base class for text box contents.

--- RPG/box_contents.cpp
+++ RPG/box_contents.cpp
@@ -4,11 +4,98 @@
 
 
 
+/*static*/ Text* Box_Contents::text_system_ = NULL;
+
+
+
 Box_Contents::Box_Contents( void ) :
-    size_{ 0,0,0,0 }
-{}
+    size_{ 0,0,0,0 },
+    text_(NULL),
+    text_qty_(0)
+{
+    if( !text_system_ )
+    {
+        text_system_ = new Text;
+    }
+}
+
+
+
+Box_Contents::Box_Contents( const Uint8_t_String& str ) :
+    Box_Contents()
+{
+    if( text_ )
+    {
+        *text_ = str;
+        return;
+    }
+    else
+    {
+        text_ = new Line_of_Text( str );
+    }
+    text_qty_ = 1;
+}
+
+
+
+Box_Contents& Box_Contents::operator+( const Uint8_t_String& rhs )
+{
+    Line_of_Text* newl = new Line_of_Text[ ++text_qty_ ];
+    for( unsigned u = 0; u < ( text_qty_ - 1 ); u++ )
+    {
+        newl[ u ] = text_[ u ];
+    }
+    newl[ text_qty_ - 1 ] = rhs;
+
+    delete[] text_;
+    text_ = newl;
+    text_ = NULL;
+
+    return *this;
+}
+
+
+
+Line_of_Text* Box_Contents::get_text( void )
+{
+    return text_;
+}
 
 
 
 Box_Contents::~Box_Contents( void )
-{}
+{
+    text_system_ = NULL;
+    if( text_ )
+    {
+        delete[] text_;
+    }
+}
+
+
+
+Line_of_Text& Box_Contents::operator[]( const unsigned& u )
+{
+    return text_[ u ];
+}
+
+
+
+Line_of_Text& Box_Contents::operator[]( const int& i )
+{
+    return this->operator[]( (unsigned)i );
+}
+
+
+
+SDL_Rect& Box_Contents::size( void )
+{
+    return size_;
+}
+
+
+
+unsigned& Box_Contents::lines( void )
+{
+    return text_qty_;
+}
--- RPG/box_contents.h
+++ RPG/box_contents.h
@@ -3,14 +3,34 @@
 #include<SDL.h>
 #include "text_line.h"
 
+class Text;
 
 
 class Box_Contents
 {
 public:
     Box_Contents( void );
+    Box_Contents( const Uint8_t_String& );
+
     ~Box_Contents( void );
 
-private:
+    Line_of_Text& operator[]( const unsigned& );
+    Line_of_Text& operator[]( const int& );
+
+    Box_Contents& operator+( const Uint8_t_String& );
+
+    SDL_Rect& size( void );
+    Line_of_Text* get_text( void );
+    unsigned& lines( void );
+
+    virtual void update( void* = NULL, const unsigned& = 0) = 0;
+
+    static void init( Text* t ) { text_system_ = t; }
+
+protected:
     SDL_Rect size_;
+    Line_of_Text* text_;
+    unsigned text_qty_;
+
+    static Text* text_system_;
 };
