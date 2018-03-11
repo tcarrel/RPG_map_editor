77c1db1cce03eb7b27b7be7d8ac9af3f83cc9e37
Thomas Russel Carrel <goshisanniichi@yahoo.com>
2018-03-11 15:29:25 -0700

    Added a class for generic text boxes.

---
+++ RPG/generic_text.cpp
@@ -0,0 +1,9 @@
+
+
+#include "stdafx.h"
+
+
+
+Generic_Text::Generic_Text( void ) :
+    Box_Contents()
+{}
---
+++ RPG/generic_text.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "box_contents.h"
+
+
+
+class Generic_Text : public Box_Contents
+{
+public:
+    Generic_Text( void );
+
+private:
+
+
+};
