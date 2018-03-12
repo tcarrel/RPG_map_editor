c80a291377378168152c37029f41c26587aeec08
Thomas Russel Carrel <goshisanniichi@yahoo.com>
2018-03-11 22:50:57 -0700

    Removed incorrect code.

--- RPG/text_selectable_list.cpp
+++ RPG/text_selectable_list.cpp
@@ -25,42 +25,7 @@ Selectable_List::~Selectable_List( void )
 
 
 
-void Selectable_List::add_text( Line_of_Text* line, bool heading )
+void Selectable_List::set_width( unsigned u )
 {
-    line_qty_;
-    if( heading )
-    {
-        ++heading_qty_;
-    }
-
-    Line_of_Text* new_text = new Line_of_Text[ line_qty_ + 1];
-    for( unsigned i = 0; i < line_qty_; i++ )
-    {
-        new_text[ i ] = text_[ i ];
-    }
-    new_text[ line_qty_ ] = *line;
-
-    if( line_qty_ > 0 )
-    {
-        delete[] text_;
-    }
-
-    text_ = new_text;
-    new_text = NULL;
-
-    ++line_qty_;
-}
-
-
-
-Line_of_Text* Selectable_List::get_text( void )
-{
-    return text_;
-}
-
-
-
-unsigned Selectable_List::size( void )
-{
-    return line_qty_;
+    size_.w = u;
 }
--- RPG/text_selectable_list.h
+++ RPG/text_selectable_list.h
@@ -4,24 +4,21 @@
 using std::vector;
 
 #include "text_line.h"
+class Box_Contents;
 
 
-class Selectable_List
+class Selectable_List : public Box_Contents
 {
 public:
     Selectable_List( void );
     ~Selectable_List( void );
 
-    void add_text( Line_of_Text*, bool );
-
-    Line_of_Text* get_text( void );
-    unsigned size( void );
+    void set_width( unsigned );
 
 private:
     unsigned heading_qty_;
     unsigned line_qty_;
     Line_of_Text* text_;
-    //bool* active_;
 
     unsigned selected_;
 };
